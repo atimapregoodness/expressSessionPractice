195b47532a54486beedbd1f58bfb09d591d3d0cd
Atimapre Goodness <atimaxgoodness@gmail.com>
2023-12-11 20:33:45 +0100

    made changes

--- index.js
+++ index.js
@@ -8,17 +8,17 @@ app.set('view engine', 'ejs')
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json())
 app.use(cookieParser())
-app.use(session({ secret: 'thisisasecret' }));
-
+app.use(session({ secret: 'thisisasecret', resave: false, saveUninitialized: false}));
 
 app.get('/view-counts', (req, res) => {
     if (req.session.count) {
-        req.session.count += 1
+        req.session.count += 1 
     } else {
         req.session.count = 1
     }
     res.send(`You have viewed this page ${req.session.count} time(s)`)
 })
+
 
 
 app.listen(3000, (req, res) => {
