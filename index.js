4991afde3df60fefe970e61d8966f5a2e3636ecd
Atimapre Goodness <atimaxgoodness@gmail.com>
2023-12-11 20:01:58 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,26 @@
+const cookieParser = require('cookie-parser')
+const express = require('express')
+const app = express()
+const session = require('express-session')
+
+app.set('view engine', 'ejs')
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json())
+app.use(cookieParser())
+app.use(session({ secret: 'thisisasecret' }));
+
+
+app.get('/view-counts', (req, res) => {
+    if (req.session.count) {
+        req.session.count += 1
+    } else {
+        req.session.count = 1
+    }
+    res.send(`You have viewed this page ${req.session.count} time(s)`)
+})
+
+
+app.listen(3000, (req, res) => {
+    console.log('LISTENING TO PORT: 3000')
+});
